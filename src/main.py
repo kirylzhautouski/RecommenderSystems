15e773555c7c4953e5a7d95f12e28fb32797aea0
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-04-01 00:26:54 +0300

    Implement minimal Dataset and Trainset

--- src/main.py
+++ src/main.py
@@ -3,3 +3,5 @@ from models.utils import Dataset
 
 if __name__ == '__main__':
     dataset = Dataset.from_csv_file('data/ratings.csv')
+    train, test = dataset.split_into_train_and_test_sets(0.999)
+    print(test)
--- src/models/utils.py
+++ src/models/utils.py
@@ -1,4 +1,5 @@
 import csv
+import random
 from collections import defaultdict
 
 
@@ -12,21 +13,47 @@ class Dataset:
         Arguments:
         `ratings` -- list of tuples (user_id, item_id, rating)
         """
-        self.ratings = ratings
+        self._ratings = ratings
 
     @classmethod
     def from_csv_file(cls, csv_file_name):
         with open(csv_file_name, 'r') as csv_file:
-            ratings = [(row[0], row[1], row[2])
-                       for row in csv.reader(csv_file)]
+            reader = csv.reader(csv_file)
+            next(reader)  # skip headers
+
+            ratings = [(int(row[0]), int(row[1]), float(row[2]))
+                       for row in reader]
 
         return cls(ratings)
 
-    def split_into_train_and_test_sets(train_share):
-        pass
+    def split_into_train_and_test_sets(self, train_share, random_state=1):
+        ratings_count = len(self._ratings)
+        trainset_ratings_count = int(train_share * ratings_count)
 
-    def get_full_trainset():
-        pass
+        random.seed(random_state)
+        indexes_for_trainset = random.sample(range(ratings_count),
+                                             trainset_ratings_count)
+        indexes_for_trainset.sort()
+
+        trainset_ratings = []
+        testset_ratings = []
+
+        for i, index_for_trainset in enumerate(indexes_for_trainset):
+            trainset_ratings.append(self._ratings[index_for_trainset])
+
+            testset_slice = None
+            if i != trainset_ratings_count - 1:
+                testset_slice = slice(index_for_trainset + 1,
+                                      indexes_for_trainset[i + 1])
+            else:
+                testset_slice = slice(index_for_trainset + 1, ratings_count)
+
+            testset_ratings.extend(self._ratings[testset_slice])
+
+        return Trainset.build_from_ratings(trainset_ratings), testset_ratings
+
+    def get_full_trainset(self):
+        return Trainset.build_from_ratings(self._ratings)
 
 
 class Trainset:
@@ -40,16 +67,24 @@ class Trainset:
         `items_ratings` -- dict where key is an item_id and value is a list of
         tuples (user_id, rating)
         """
-        self.users_ratings = users_ratings
-        self.items_ratings = items_ratings
+        self._users_ratings = users_ratings
+        self._items_ratings = items_ratings
 
     @classmethod
     def build_from_ratings(cls, ratings):
         users_ratings = defaultdict(list)
         items_ratings = defaultdict(list)
 
-        for (user_id, item_id, rating) in ratings:
+        for user_id, item_id, rating in ratings:
             users_ratings[user_id].append((item_id, rating))
             items_ratings[item_id].append((user_id, rating))
 
         return cls(users_ratings, items_ratings)
+
+    @property
+    def users_ratings(self):
+        return self._users_ratings
+
+    @property
+    def items_ratings(self):
+        return self._items_ratings
