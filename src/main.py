2f6f2b4f4eeaa6c50570dae09a5fedbff45a6fd8
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-04-02 13:50:09 +0300

    Translate original users and items ids to inner to be able to use them as continious integer indexes in arrays

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+from models.utils import Dataset
 from models.knn import KNN
 
 
 if __name__ == '__main__':
+    dataset = Dataset.from_csv_file('data/ratings.csv')
+
+    trainset, testset = dataset.split_into_train_and_test_sets(0.7)
+
     knn = KNN()
-    print(knn.predict(1, 1))
+    knn.fit(trainset)
+    knn.predict(1, 1)
--- src/models/utils.py
+++ src/models/utils.py
@@ -68,8 +68,12 @@ class Dataset:
         return Trainset.build_from_ratings(self._ratings)
 
 
+class UnknownIdError(Exception):
+    pass
+
+
 class Trainset:
-    def __init__(self, users_ratings, items_ratings):
+    def __init__(self, users_ratings, items_ratings, original_to_inner_users, original_to_inner_items):
         """
         Constructs `Trainset`, that is used for training model
 
@@ -85,16 +89,40 @@ class Trainset:
         self._users_count = len(users_ratings)
         self._items_count = len(items_ratings)
 
+        self._original_to_inner_users = original_to_inner_users
+        self._original_to_inner_items = original_to_inner_items
+
+        self._inner_to_original_users = {inner_id: original_id
+                                         for original_id, inner_id in original_to_inner_users.items()}
+
+        self._inner_to_original_items = {inner_id: original_id
+                                         for original_id, inner_id in original_to_inner_items.items()}
+
     @classmethod
     def build_from_ratings(cls, ratings):
         users_ratings = defaultdict(list)
         items_ratings = defaultdict(list)
 
-        for user_id, item_id, rating in ratings:
-            users_ratings[user_id].append((item_id, rating))
-            items_ratings[item_id].append((user_id, rating))
+        current_id_user, current_id_item = 0, 0
+        original_to_inner_users = {}
+        original_to_inner_items = {}
 
-        return cls(users_ratings, items_ratings)
+        for user_id, item_id, rating in ratings:
+            if user_id not in original_to_inner_users:
+                original_to_inner_users[user_id] = current_id_user
+                current_id_user += 1
+
+            if item_id not in original_to_inner_items:
+                original_to_inner_items[item_id] = current_id_item
+                current_id_item += 1
+
+            user_inner_id = original_to_inner_users[user_id]
+            item_inner_id = original_to_inner_items[item_id]
+
+            users_ratings[user_inner_id].append((item_inner_id, rating))
+            items_ratings[item_inner_id].append((user_inner_id, rating))
+
+        return cls(users_ratings, items_ratings, original_to_inner_users, original_to_inner_items)
 
     @property
     def users_ratings(self):
@@ -111,3 +139,15 @@ class Trainset:
     @property
     def items_count(self):
         return self._items_count
+
+    def to_inner_user_id(self, original_user_id):
+        if original_user_id in self._original_to_inner_users:
+            return self._original_to_inner_users[original_user_id]
+        else:
+            raise UnknownIdError(f"User with {original_user_id} wasn't in the train set")
+
+    def to_inner_item_id(self, original_item_id):
+        if original_item_id in self._original_to_inner_items:
+            return self._original_to_inner_items[original_item_id]
+        else:
+            raise UnknownIdError(f"Item with {original_item_id} wasn't in the train set")
